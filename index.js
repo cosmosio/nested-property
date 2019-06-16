a05b9e2d837000f0c5a4c627f45a9329d74974e1
Igor Barbosa <igor.barbosa.dev@gmail.com>
2019-06-16 16:05:33 -0300

    fixing a bug in the set method (it is necessary to isolate a condition with parentheses).

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ function setNestedProperty(object, property, value) {
             var split = property.split(".");
             return split.reduce(function (obj, prop, idx) {
                 const nextPropIsNumber = Number.isInteger(Number.parseInt(split[idx+1]))
-                obj[prop] = obj[prop] || nextPropIsNumber ? [] : {}
+                obj[prop] = obj[prop] || (nextPropIsNumber ? [] : {})
                 if (split.length == (idx + 1)) {
                     obj[prop] = value;
                 }

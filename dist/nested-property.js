a47d0c98d5052e32c733fc8a7fc1cf15c2b53b2e
Olivier Scherrer <pode.fr@gmail.com>
2020-09-27 10:56:50 -0400

    Prepare for 3.0.0

--- dist/nested-property.js
+++ dist/nested-property.js
@@ -9,8 +9,47 @@
 
 function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
 
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _wrapNativeSuper(Class) { var _cache = typeof Map === "function" ? new Map() : undefined; _wrapNativeSuper = function _wrapNativeSuper(Class) { if (Class === null || !_isNativeFunction(Class)) return Class; if (typeof Class !== "function") { throw new TypeError("Super expression must either be null or a function"); } if (typeof _cache !== "undefined") { if (_cache.has(Class)) return _cache.get(Class); _cache.set(Class, Wrapper); } function Wrapper() { return _construct(Class, arguments, _getPrototypeOf(this).constructor); } Wrapper.prototype = Object.create(Class.prototype, { constructor: { value: Wrapper, enumerable: false, writable: true, configurable: true } }); return _setPrototypeOf(Wrapper, Class); }; return _wrapNativeSuper(Class); }
+
+function isNativeReflectConstruct() { if (typeof Reflect === "undefined" || !Reflect.construct) return false; if (Reflect.construct.sham) return false; if (typeof Proxy === "function") return true; try { Date.prototype.toString.call(Reflect.construct(Date, [], function () {})); return true; } catch (e) { return false; } }
+
+function _construct(Parent, args, Class) { if (isNativeReflectConstruct()) { _construct = Reflect.construct; } else { _construct = function _construct(Parent, args, Class) { var a = [null]; a.push.apply(a, args); var Constructor = Function.bind.apply(Parent, a); var instance = new Constructor(); if (Class) _setPrototypeOf(instance, Class.prototype); return instance; }; } return _construct.apply(null, arguments); }
+
+function _isNativeFunction(fn) { return Function.toString.call(fn).indexOf("[native code]") !== -1; }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+
 var ARRAY_WILDCARD = "+";
 var PATH_DELIMITER = ".";
+
+var ObjectPrototypeMutationError =
+/*#__PURE__*/
+function (_Error) {
+  _inherits(ObjectPrototypeMutationError, _Error);
+
+  function ObjectPrototypeMutationError(params) {
+    var _this;
+
+    _classCallCheck(this, ObjectPrototypeMutationError);
+
+    _this = _possibleConstructorReturn(this, _getPrototypeOf(ObjectPrototypeMutationError).call(this, params));
+    _this.name = "ObjectPrototypeMutationError";
+    return _this;
+  }
+
+  return ObjectPrototypeMutationError;
+}(_wrapNativeSuper(Error));
+
 module.exports = {
   set: setNestedProperty,
   get: getNestedProperty,
@@ -20,7 +59,8 @@ module.exports = {
       own: true
     });
   },
-  isIn: isInNestedProperty
+  isIn: isInNestedProperty,
+  ObjectPrototypeMutationError: ObjectPrototypeMutationError
 };
 /**
  * Get the property of an object nested in one or more objects or array
@@ -125,6 +165,10 @@ function setNestedProperty(object, property, value) {
 
   try {
     return traverse(object, property, function _setNestedProperty(currentObject, currentProperty, segments, index) {
+      if (currentObject === Reflect.getPrototypeOf({})) {
+        throw new ObjectPrototypeMutationError("Attempting to mutate Object.prototype");
+      }
+
       if (!currentObject[currentProperty]) {
         var nextPropIsNumber = Number.isInteger(Number(segments[index + 1]));
         var nextPropIsArrayWildcard = segments[index + 1] === ARRAY_WILDCARD;
@@ -143,7 +187,12 @@ function setNestedProperty(object, property, value) {
       return currentObject[currentProperty];
     });
   } catch (err) {
-    return object;
+    if (err instanceof ObjectPrototypeMutationError) {
+      // rethrow
+      throw err;
+    } else {
+      return object;
+    }
   }
 }
 /**
